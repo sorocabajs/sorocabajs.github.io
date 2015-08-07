92d5dd2c18ff90743b594f920b54d1d2db4e4a78
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-08-07 12:18:41 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sorocabajs.github.io
-Our website
+
+SorocabaJS é um grupo de encontro para que os entusiastas de JavaScript de Sorocaba e região possam se reunir, programar e trocar experiência desde os mais experientes aos novatos! O SorocabaJS tem como objetivo criar espaços sociais de integração para fortalecer as comunidades locais de software.
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    minifyCSS   = require('gulp-minify-css'),
+    prefixer    = require('gulp-autoprefixer'),
+    ghPages     = require('gulp-gh-pages'),
+    img         = require('gulp-imagemin'),
+    minifyHTML = require('gulp-minify-html');
+
+gulp.task ('default', ['styles', 'img', 'html']);
+
+gulp.task ('styles', function () {
+  return gulp
+  .src('./src/sass/**/*.sass')
+  .pipe(sass({
+    indentedSyntax: true
+  }))
+  .pipe(prefixer())
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('./build/assets/css/'));
+});
+
+gulp.task('html', function() {
+  return gulp
+  .src('./src/html/*')
+  .pipe(minifyHTML())
+  .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('img', function() {
+  return gulp
+  .src('./src/img/*')
+  .pipe(img({
+    progressive: true
+  }))
+  .pipe(gulp.dest('./build/assets/img/'));
+});
+
+gulp.task('deploy', ['default'], function() {
+  return gulp
+  .src('./build/**/*')
+  .pipe(ghPages({
+    branch: 'master'
+  }));
+});
+
+gulp.task ('watch', ['default'], function () {
+  gulp.watch('./src/js/**/*', ['scripts']);
+  gulp.watch('./src/sass/**/*', ['styles']);
+  gulp.watch('./src/html/*', ['html']);
+});
