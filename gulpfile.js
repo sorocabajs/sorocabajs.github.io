c90ef9e8df3aa27d0db2c43a48d2381989ab5564
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-11-03 20:09:45 -0200

    Add new site for second event

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp        = require('gulp'),
     prefixer    = require('gulp-autoprefixer'),
     ghPages     = require('gulp-gh-pages'),
     img         = require('gulp-imagemin'),
-    minifyHTML = require('gulp-minify-html');
+    minifyHTML  = require('gulp-minify-html'),
+    connect     = require('gulp-connect')
 
-gulp.task ('default', ['styles', 'img', 'html']);
+gulp.task('default', ['styles', 'img', 'html'])
 
-gulp.task ('styles', function () {
+gulp.task('styles', function () {
   return gulp
   .src('./src/sass/**/*.sass')
   .pipe(sass({
@@ -16,35 +17,42 @@ gulp.task ('styles', function () {
   }))
   .pipe(prefixer())
   .pipe(minifyCSS())
-  .pipe(gulp.dest('./build/assets/css/'));
-});
+  .pipe(gulp.dest('./build/assets/css/'))
+})
 
 gulp.task('html', function() {
   return gulp
   .src('./src/html/*')
   .pipe(minifyHTML())
-  .pipe(gulp.dest('./build/'));
-});
+  .pipe(gulp.dest('./build/'))
+})
 
 gulp.task('img', function() {
   return gulp
-  .src('./src/img/*')
+  .src('./src/img/**/*')
   .pipe(img({
     progressive: true
   }))
-  .pipe(gulp.dest('./build/assets/img/'));
-});
+  .pipe(gulp.dest('./build/assets/img/'))
+})
 
 gulp.task('deploy', ['default'], function() {
   return gulp
   .src('./build/**/*')
   .pipe(ghPages({
     branch: 'master'
-  }));
-});
+  }))
+})
 
-gulp.task ('watch', ['default'], function () {
-  gulp.watch('./src/js/**/*', ['scripts']);
-  gulp.watch('./src/sass/**/*', ['styles']);
-  gulp.watch('./src/html/*', ['html']);
-});
+gulp.task('watch', ['default'], function () {
+  gulp.watch('./src/js/**/*', ['scripts'])
+  gulp.watch('./src/sass/**/*', ['styles'])
+  gulp.watch('./src/html/*', ['html'])
+})
+
+gulp.task('serve', ['watch'], function () {
+  connect.server({
+    root: './build/',
+    livereload: true
+  })
+})
